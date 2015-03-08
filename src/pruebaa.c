2032d6d278083690acf9a56ed1227ad8a4c06ecb
rmejiadelagala <rmejiadelagala@gmail.com>
2015-03-08 00:39:25 -0300

    mundo cruel

--- src/pruebaa.c
+++ src/pruebaa.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hola Mundo!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hola Mundo Cruel!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
