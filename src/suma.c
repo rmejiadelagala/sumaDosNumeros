b44fdfaccacffacd71cdac022f832519d84d4ebd
rmejiadelagala <rmejiadelagala@gmail.com>
2015-03-08 00:48:23 -0300

    se reemplaza codigo original de prueba y se coloca el real. Se solicita por consola 2 numeros, y se devuelve la suma de ambos. Tan facil y basico como eso!!!

--- src/suma.c
+++ src/suma.c
@@ -8,10 +8,15 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-	puts("!!!Hola Mundo Cruel!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+# include <stdio.h>
+int main ()
+{
+	int valor1, valor2;
+	printf("Hola este es un ensayo!!!\n");
+	printf("ingrese primer valor entero:\n ");
+	scanf("%d",&valor1);
+	printf("ingrese  valor entero:\n");
+	scanf("%d",&valor2);
+	printf("el resultado de la suma es: %d \n",valor1+valor2);
+	return 1;
 }
